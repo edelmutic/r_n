21ece56a5c404524dc3811c888862002489c6937
edelmutic <edelmutic@gmail.com>
2021-04-26 20:57:40 +0300

    add useEffect hooj and fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
+import axios from 'axios';
 
 function App() {
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      email: 'Sincere@april.biz',
-      address: {
-        street: 'Kulas Light',
-        city: 'Gwenborough',
-        zipcode: '92998-3874',
-      },
-    },
-    {
-      id: 2,
-      name: 'Leanne Graham',
-      email: 'Sincere@april.biz',
-      address: {
-        street: 'Kulas Light',
-        city: 'Gwenborough',
-        zipcode: '92998-3874',
-      },
-    },
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get<IUser[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setUsers(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
     <div>
       <Card
