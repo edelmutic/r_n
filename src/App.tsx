744d0bcbfe9eebf0b3faf01f4d0a6b683431a06b
edelmutic <edelmutic@gmail.com>
2021-04-26 20:26:22 +0300

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 function App() {
+  const users: IUser[] = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+      },
+    },
+    {
+      id: 2,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+      },
+    },
+  ];
   return (
     <div>
       <Card
@@ -13,6 +37,7 @@ function App() {
       >
         <button>Send</button>
       </Card>
+      <UserList users={users} />
     </div>
   );
 }
