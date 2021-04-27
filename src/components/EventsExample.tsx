73877563eaaadf9637bc339cdd3416bc0b85e995
edelmutic <edelmutic@gmail.com>
2021-04-27 13:38:24 +0300

    uodate dragable component

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -4,12 +4,33 @@ import React, { FC, useState } from 'react';
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>('');
+  const [isDrag, setIsDrag] = useState<boolean>(false);
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   const clickHandler = (e: React.MouseEvent) => {
     console.log(value);
+  };
+
+  const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('DRAG');
+  };
+
+  const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+  };
+
+  const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+  };
+
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+    console.log('DROP');
   };
   return (
     <div>
