aea54ddaed45fd982d110835d9f08f3c6dd30e98
edelmutic <edelmutic@gmail.com>
2021-04-27 14:56:14 +0300

    add exact routes

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,22 @@ import UserItem from './components/UserItem';
 import TodoItem from './components/TodoItem';
 import EventsExample from './components/EventsExample';
 import { BrowserRouter, Route } from 'react-router-dom';
+import UserPage from './components/UserPage';
+import TodosPage from './components/TodosPage';
 
 function App() {
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [todos, setTodos] = useState<ITodo[]>([]);
-  useEffect(() => {
-    fetchUsers();
-    fetchTodos();
-  }, []);
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get<IUser[]>(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      setUsers(response.data);
-    } catch (error) {
-      alert(error);
-    }
-  };
-  const fetchTodos = async () => {
-    try {
-      const response = await axios.get<ITodo[]>(
-        'https://jsonplaceholder.typicode.com/todos?_limit=10'
-      );
-      setTodos(response.data);
-    } catch (error) {
-      alert(error);
-    }
-  };
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path={'/users'} exact>
+          <UserPage />
+        </Route>
+        <Route path={'/todos'} exact>
+          <TodosPage />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
